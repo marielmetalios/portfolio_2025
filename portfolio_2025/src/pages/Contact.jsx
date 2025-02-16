119c96c9c8f39aba5ec5cb43f0cbb3c520b1c4e9
Your Name <marielmetalios@gmail.com>
2025-02-16 15:41:07 -0500

    updated contact form

--- portfolio_2025/src/pages/Contact.jsx
+++ portfolio_2025/src/pages/Contact.jsx
@@ -1,7 +1,49 @@
+import { useState } from "react";
+
 const Contact = () => {
+    const [formInfo, setFormInfo] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+
+    // boolean for submitted, set to false initiallity
+    const [submitted, setSubmitted] = useState(false)
+
+    function handleChange(event) {
+        setFormInfo({ ...formInfo, [event.target.name]: event.target.value });
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        if (!formInfo.name || !formInfo.email || !formInfo.message) {
+            console.log('fields not filled out')
+        } else {
+            console.log('form submitted', formInfo)
+            setSubmitted(true);
+        }
+
+        localStorage.setItem('form info', JSON.stringify(formInfo));
+        setFormInfo({name: '', email: '', message: ''});
+    }
+
+
+
     return (
         <div>
-            <h2>Contact Me Page Here</h2>
+            <h2>Contact Form</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Name</label>
+                <input id="name" name="name" type="text" value={formInfo.name} onChange={handleChange}>
+                </input>
+                <label htmlFor="email">Email</label>
+                <input id="email" name="email" type="text" value={formInfo.email} onChange={handleChange}>
+                </input>
+                <label htmlFor="message">Message</label>
+                <textarea id="message" name="message" value={formInfo.message} onChange={handleChange}>
+                </textarea>
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
