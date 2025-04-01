8fd62305e4156f00ece64f8f73e44e3fc3f85ff8
Your Name <marielmetalios@gmail.com>
2025-04-01 16:47:41 -0400

    updated resume

--- portfolio_2025/src/pages/Resume.jsx
+++ portfolio_2025/src/pages/Resume.jsx
@@ -1,10 +1,10 @@
-import pdf from "../assets/resume.pdf"
+import pdf from "../assets/mariel_resume2025.pdf"
 
 const Resume = () => {
 
 return (
     <div className="text-3xl font-bold text-left text-gray-800">
-        <a href={pdf} download="resume.pdf" >Click here to download</a>
+        <a href={pdf} download="mariel_resume2025.pdf" >Click here to download</a>
     </div>
 );
 
